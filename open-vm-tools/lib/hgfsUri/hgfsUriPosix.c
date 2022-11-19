4ec7af3d10c00a4d2214d7d490b7e635575333a0
rgc2000 <rene@margar.fr>
2022-11-19 18:39:15 +0100

    hgfsPosix.c has to be compiled also for Solaris

--- open-vm-tools/lib/hgfsUri/hgfsUriPosix.c
+++ open-vm-tools/lib/hgfsUri/hgfsUriPosix.c
@@ -23,7 +23,7 @@
  *    x-vmware-share:// style URIs
  */
 
-#if !defined __linux__ && !defined __APPLE__ && !defined __FreeBSD__
+#if !defined __linux__ && !defined __APPLE__ && !defined __FreeBSD__ && !defined __sun__
 #   error This file should not be compiled
 #endif
 
