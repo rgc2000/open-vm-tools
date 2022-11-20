8ca7ef4262a2f0e4f63580b5031832dc9e551f0e
rgc2000 <rene@margar.fr>
2022-11-20 01:07:58 +0100

    Build also desktop tools for Solaris

--- solaris-build/build.sh
+++ solaris-build/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/ksh
-# Build script to create Solatis 11 pkg files for open-vm-tools
+# Build script to create Solatis 11 pkg file for open-vm-tools
 
 umask 022
+
 export DESTDIR=/tmp/vmtools-build
 export LD_RUN_PATH=/opt/vmware/lib
 export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig
@@ -41,7 +42,7 @@ case "${TARGETOS}" in
 
         cd ../open-vm-tools
         autoreconf -i
-        ./configure --prefix=/opt/vmware --disable-static --without-x --enable-libappmonitor CFLAGS="-I$DESTDIR/opt/vmware/include -L$DESTDIR/opt/vmware/lib $OPTION -Wno-unused-variable"
+	./configure --prefix=/opt/vmware --disable-static --enable-libappmonitor CFLAGS="-I$DESTDIR/opt/vmware/include -L$DESTDIR/opt/vmvmware/lib $OPTION -Wno-unused-variable"
         gmake -j 5
         gmake install
         cd ../solaris-build
@@ -53,13 +54,18 @@ case "${TARGETOS}" in
         mkdir -p $DESTDIR/usr/lib/fs/vmblock
         mkdir -p $DESTDIR/usr/lib/fs/vmhgfs
         mkdir -p $DESTDIR/etc/fs
+        mkdir -p $DESTDIR/etc/xdg/autostart
         ln -fs /opt/vmware/sbin/mount.vmblock $DESTDIR/usr/lib/fs/vmblock/mount
         ln -fs /opt/vmware/sbin/mount.vmhgfs $DESTDIR/usr/lib/fs/vmhgfs/mount
         ln -fs /usr/lib/fs/vmblock $DESTDIR/etc/fs/vmblock
         ln -fs /usr/lib/fs/vmhgfs $DESTDIR/etc/fs/vmhgfs
         ln -fs /opt/vmware/bin/vmtoolsd $DESTDIR/usr/sbin/vmtoolsd
         ln -fs /opt/vmware/bin/vmware-toolbox-cmd $DESTDIR/usr/bin/vmware-toolbox-cmd
+        ln -fs /opt/vmware/etc/xdg/autostart/vmware-user.desktop $DESTDIR/etc/xdg/autostart/vmware-user.desktop
         rm -f $DESTDIR/usr/sbin/mount.*
+        [ -f $DESTDIR/etc/pam.d/vmtoolsd ] && chmod -x $DESTDIR/etc/pam.d/vmtoolsd
+        [ -f $DESTDIR/opt/vmware/bin/vmware-user-suid-wrapper ] && chmod u+s $DESTDIR/opt/vmware/bin/vmware-user-suid-wrapper
+        chmod -x $DESTDIR/opt/vmware/lib/lib*
 
         # =======  Sign solaris drivers
 
@@ -79,24 +85,17 @@ case "${TARGETOS}" in
 
         cp -pr proto/etc "$DESTDIR"
 
-        pkgsend generate $DESTDIR | pkgfmt |
-            grep -v 'dir  path=etc ' |
-            grep -v 'dir  path=etc/fs ' |
-            grep -v 'dir  path=etc/pam.d ' |
-            grep -v 'dir  path=etc/certs' |
-            grep -v 'dir  path=kernel ' |
-            grep -v 'dir  path=kernel/drv ' |
-            grep -v 'dir  path=kernel/drv/amd64 ' |
-            grep -v 'dir  path=opt ' |
-            grep -v 'dir  path=usr ' |
-            grep -v 'dir  path=usr/lib ' |
-            grep -v 'dir  path=usr/lib/fs ' |
-            grep -v 'dir  path=usr/sbin ' |
-            grep -v 'dir  path=usr/bin ' > open-vm-tools.p5m.1
+        pkgsend generate $DESTDIR | pkgfmt > open-vm-tools.p5m.1
 
         cp -pr proto/lib "$DESTDIR"
 
         cat open-vm-tools-header.p5m open-vm-tools.p5m.1 open-vm-tools-footer.p5m > open-vm-tools.p5m
+
+        for dir in etc etc/certs etc/certs/elfsign etc/fs etc/pam.d etc/xdg etc/xdg/autostart kernel kernel/drv kernel/drv/amd64 opt usr usr/lib/fs
+        do
+            sed "s,path=$dir owner=root group=bin,path=$dir owner=root group=sys," open-vm-tools.p5m > open-vm-tools.p5m.a &&
+	    mv open-vm-tools.p5m.a open-vm-tools.p5m
+        done
 
         rm -rf my-repository
         mkdir my-repository
