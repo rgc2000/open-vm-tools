20a6f62afbff193fd2d78eb385aa0c23399b26e7
rgc2000 <rene@margar.fr>
2022-11-30 20:04:23 +0100

    Configurables and relatives pathes in Solaris build script

--- solaris-build/build.sh
+++ solaris-build/build.sh
@@ -3,8 +3,11 @@
 
 umask 022
 
+export PREFIX=/opt/vmware
+export SYSCONFDIR=${PREFIX}/etc
+export LIBDIR=${PREFIX}/lib
 export DESTDIR=/tmp/vmtools-build
-export LD_RUN_PATH=/opt/vmware/lib
+export LD_RUN_PATH=${LIBDIR}
 export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig
 
 TARGETOS=$(uname -s)
@@ -27,52 +30,56 @@ case "${TARGETOS}" in
 
         cd ../open-vm-tools
         autoreconf -i
-        ./configure --prefix=/opt/vmware --disable-static --enable-libappmonitor
+        ./configure --prefix=${PREFIX} --sysconfdir=${SYSCONFDIR} --libdir=${LIBDIR} --disable-static --enable-libappmonitor
         gmake -j 5
         gmake install
         cd ../solaris-build
 
         # =======  Clean files
 
-        find $DESTDIR -type f -exec file {} \; | grep 'not stripped' | cut -d: -f1 | xargs -t -L 1 strip
-        mkdir -p $DESTDIR/usr/sbin
-        mkdir -p $DESTDIR/usr/lib/fs/vmblock
-        mkdir -p $DESTDIR/usr/lib/fs/vmhgfs
-        mkdir -p $DESTDIR/etc/fs
-        mkdir -p $DESTDIR/etc/xdg/autostart
-        ln -fs /opt/vmware/sbin/mount.vmblock $DESTDIR/usr/lib/fs/vmblock/mount
-        ln -fs /opt/vmware/sbin/mount.vmhgfs $DESTDIR/usr/lib/fs/vmhgfs/mount
-        ln -fs /usr/lib/fs/vmblock $DESTDIR/etc/fs/vmblock
-        ln -fs /usr/lib/fs/vmhgfs $DESTDIR/etc/fs/vmhgfs
-        ln -fs /opt/vmware/bin/vmtoolsd $DESTDIR/usr/sbin/vmtoolsd
-        ln -fs /opt/vmware/bin/vmware-toolbox-cmd $DESTDIR/usr/bin/vmware-toolbox-cmd
-        ln -fs /opt/vmware/etc/xdg/autostart/vmware-user.desktop $DESTDIR/etc/xdg/autostart/vmware-user.desktop
-        rm -f $DESTDIR/usr/sbin/mount.*
-        [ -f $DESTDIR/etc/pam.d/vmtoolsd ] && chmod -x $DESTDIR/etc/pam.d/vmtoolsd
-        [ -f $DESTDIR/opt/vmware/bin/vmware-user-suid-wrapper ] && chmod u+s $DESTDIR/opt/vmware/bin/vmware-user-suid-wrapper
-        chmod -x $DESTDIR/opt/vmware/lib/lib*
+        find ${DESTDIR} -type f -exec file {} \; | grep 'not stripped' | cut -d: -f1 | xargs -t -L 1 strip
+
+        if [ "${PREFIX}" != "/usr" ]
+        then
+            [ -d "${DESTDIR}/usr/bin" ] || mkdir -p "${DESTDIR}/usr/bin"
+            ln -fs $(realpath --relative-to=/usr/bin "${PREFIX}/bin/vmtoolsd") "${DESTDIR}/usr/bin/vmtoolsd"
+            ln -fs $(realpath --relative-to=/usr/bin "${PREFIX}/bin/vmware-toolbox-cmd") "${DESTDIR}/usr/bin/vmware-toolbox-cmd"
+        fi
+
+        if [ "${SYSCONFDIR}" != "/etc" ]
+        then
+            [ -d "${DESTDIR}/etc/xdg/autostart" ] || mkdir -p "${DESTDIR}/etc/xdg/autostart"
+            ln -fs $(realpath --relative-to=/etc/xdg/autostart "${SYSCONFDIR}/xdg/autostart/vmware-user.desktop") "${DESTDIR}/etc/xdg/autostart/vmware-user.desktop"
+        fi
+
+        [ -d "${DESTDIR}/etc/fs" ] || mkdir -p "${DESTDIR}/etc/fs"
+        ln -fs $(realpath --relative-to=/etc/fs "${LIBDIR}/fs/vmblock") "${DESTDIR}/etc/fs/vmblock"
+        ln -fs $(realpath --relative-to=/etc/fs "${LIBDIR}/fs/vmhgfs") "${DESTDIR}/etc/fs/vmhgfs"
+        [ -f "${DESTDIR}/etc/pam.d/vmtoolsd" ] && chmod -x "${DESTDIR}/etc/pam.d/vmtoolsd"
+        [ -f "${DESTDIR}${PREFIX}/bin/vmware-user-suid-wrapper" ] && chmod u+s "${DESTDIR}${PREFIX}/bin/vmware-user-suid-wrapper"
+        chmod -x ${DESTDIR}${LIBDIR}/lib*
 
         # =======  Sign solaris drivers
 
         ./new-cert.sh
 
-        for i in $DESTDIR/kernel/drv/amd64/*
+        for i in ${DESTDIR}/kernel/drv/amd64/*
         do
             echo "signing $i"
             elfsign sign -F rsa_sha256 -c open-vm-tools.crt -k open-vm-tools.key -e $i
             chmod -wx "$i"
         done
 
-        mkdir -p "$DESTDIR/etc/certs/elfsign"
-        cp -p open-vm-tools.crt "$DESTDIR/etc/certs/elfsign"
+        mkdir -p "${DESTDIR}/etc/certs/elfsign"
+        cp -p open-vm-tools.crt "${DESTDIR}/etc/certs/elfsign"
 
         # =======  Create pkg files manifest
 
-        cp -pr proto/etc "$DESTDIR"
+        cp -pr proto/etc "${DESTDIR}"
 
-        pkgsend generate $DESTDIR | pkgfmt > open-vm-tools.p5m.1
+        pkgsend generate ${DESTDIR} | pkgfmt > open-vm-tools.p5m.1
 
-        cp -pr proto/lib "$DESTDIR"
+        cp -pr proto/lib "${DESTDIR}"
 
         cat open-vm-tools-header.p5m open-vm-tools.p5m.1 open-vm-tools-footer.p5m > open-vm-tools.p5m
 
@@ -82,11 +89,11 @@ case "${TARGETOS}" in
 	    mv open-vm-tools.p5m.a open-vm-tools.p5m
         done
 
-        rm -rf my-repository
+        rm -rf my-repository open-vm-tools.p5p
         mkdir my-repository
         pkgrepo create my-repository
         pkgrepo -s my-repository add-publisher community
-        pkgsend publish -d "$DESTDIR" -s ./my-repository open-vm-tools.p5m
+        pkgsend publish -d "${DESTDIR}" -s ./my-repository open-vm-tools.p5m
         pkgrecv -s my-repository -a -d open-vm-tools.p5p vmware/open-vm-tools
 
         echo "Compilation for Solaris finished, pkg is open-vm-tools.p5p"
