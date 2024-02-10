bbe91a6c8e40b1dc3b0f0fb634eb721d9fcf551a
Rene Garcia <rene@margar.fr>
2024-02-10 23:34:07 +0100

    Clean MacOS build script

--- macos-build/build.sh
+++ macos-build/build.sh
@@ -3,8 +3,8 @@
 
 umask 022
 
-export PREFIX=/usr/local
-export SYSCONFDIR=/etc
+export PREFIX="/usr/local"
+export SYSCONFDIR="/etc"
 export LIBDIR=${PREFIX}/lib
 export DESTDIR=/tmp/vmtools-build
 export LD_RUN_PATH=${LIBDIR}
@@ -19,23 +19,14 @@ case "${TARGETOS}" in
 
         cd ../open-vm-tools
         autoreconf -i
-        ./configure --enable-silent-rules --prefix=${PREFIX} --sysconfdir=${SYSCONFDIR} --libdir=${LIBDIR} --disable-static --enable-libappmonitor --without-x --disable-resolutionkms --enable-vmwgfxctrl --enable-vgauth
+        ./configure --enable-silent-rules --prefix="${PREFIX}" --sysconfdir="${SYSCONFDIR}" --libdir="${LIBDIR}" --disable-static --enable-libappmonitor --without-x --enable-resolutionkms --enable-vgauth
         make -j 5
         make install
         cd ../macos-build
 
         # =======  Clean files
 
-        find ${DESTDIR} -type f -exec file {} \; | grep 'not stripped' | cut -d: -f1 | xargs -t -L 1 strip
-
-        if [ "${SYSCONFDIR}" != "/etc" ]
-        then
-            [ -d "${DESTDIR}/etc/fs" ] || mkdir -p "${DESTDIR}/etc/fs"
-            [ -d "${DESTDIR}/etc/xdg/autostart" ] || mkdir -p "${DESTDIR}/etc/xdg/autostart"
-            ln -fs $(realpath --relative-to=/etc/xdg/autostart "${SYSCONFDIR}/xdg/autostart/vmware-user.desktop") "${DESTDIR}/etc/xdg/autostart/vmware-user.desktop"
-            ln -fs $(realpath --relative-to=/etc/fs "${LIBDIR}/fs/vmblock") "${DESTDIR}/etc/fs/vmblock"
-            ln -fs $(realpath --relative-to=/etc/fs "${LIBDIR}/fs/vmhgfs") "${DESTDIR}/etc/fs/vmhgfs"
-        fi
+        find ${DESTDIR} -type f -exec file {} \; | grep 'Mach-O' | cut -d: -f1 | xargs -t -L 1 strip -x -S
 
         [ -f "${DESTDIR}/etc/pam.d/vmtoolsd" ] && chmod -x "${DESTDIR}/etc/pam.d/vmtoolsd"
         [ -f "${DESTDIR}${PREFIX}/bin/vmware-user-suid-wrapper" ] && chmod u+s "${DESTDIR}${PREFIX}/bin/vmware-user-suid-wrapper"
